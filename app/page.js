1bddfad825e35b5012eb1618f268550fb2c8f1a0
Professor <ngohuutuan221020@gmail.com>
2024-02-08 19:19:25 +0700

    update

--- app/page.js
+++ app/page.js
@@ -4,11 +4,23 @@ const getData = async () => {
   const resp = await fetch("https://ngohuutuan221020.github.io/api/video.json");
   return resp.json();
 };
+const getData2 = async () => {
+  const resp = await fetch("https://api.sampleapis.com/coffee/hot");
+  return resp.json();
+};
 export default async function Home() {
   const api = await getData();
+  const api2 = await getData2();
 
   return (
     <main className={styles.main}>
+      {api2.map((item, index) => {
+        return (
+          <>
+            <div>{item.title}</div>
+          </>
+        );
+      })}
       <h1 style={{marginBottom: "2rem"}}>
         <img height={50} src="https://logos-world.net/wp-content/uploads/2023/08/X-Logo.png"></img>Video
       </h1>
